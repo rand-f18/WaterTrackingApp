598db20eb9f8b89dbdae36b87e3e6c3734a631de
Rand <randalobaid9@gmail.com>
2024-10-20 15:44:42 +0300

    Initial Commit

---
+++ Hydrate/HydrateApp.swift
@@ -0,0 +1,17 @@
+//
+//  HydrateApp.swift
+//  Hydrate
+//
+//  Created by Rand Soliman Alobaid on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct HydrateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
